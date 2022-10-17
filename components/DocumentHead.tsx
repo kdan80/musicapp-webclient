5e363b3423607f5c9864bdcab775e11bddf309df
kdan80 <kierandansey80@gmail.com>
2022-10-17 19:10:07 +0100

    Added favicons

--- components/DocumentHead.tsx
+++ components/DocumentHead.tsx
@@ -8,12 +8,13 @@ const DocumentHead = () => {
         <Head>
             <meta charSet='utf-8' />
             <title>MusicApp</title>
+            <link rel='manifest' href='/manifest.json' />
             <meta name='description' content='A music streaming app built with NextJS' />
             <meta name='theme-color' content='#101010'/>
-            <link rel='icon' href='/images/favicons/favicon.ico?v=1.0' />
-            <link rel='apple-touch-icon' sizes='180x180' href='/images/favicons/apple-touch-icon.png' />
-            <link rel='icon' type='image/png' sizes='16x16' href='/images/favicons/favicon-16x16.png' />
-            <link rel='icon' type='image/png' sizes='32x32' href='/images/favicons/favicon-32x32.png' />
+            <link rel='icon' href='/favicon.ico?v=1.0' />
+            <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+            <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+            <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
         </Head>
     );
 };
