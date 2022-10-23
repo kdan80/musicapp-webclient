9f603e5e7adb21142c25e81dec5903fb2df993de
kdan80 <kierandansey80@gmail.com>
2022-10-23 19:00:04 +0100

    typo

--- components/AudioPlayer/MiniPlayer.tsx
+++ components/AudioPlayer/MiniPlayer.tsx
@@ -58,7 +58,7 @@ const MiniPlayer: React.FC<Props> = ({
                                 height={50}
                                 width={50} 
                                 //src={`https://d30if2vv974pn3.cloudfront.net/${nowPlaying!.album._id}.webP`}
-                                src={`/albumart/${nowPlaying!.album._id}.webP`}
+                                src={`/albumArt/${nowPlaying!.album._id}.webP`}
                                 alt='album art' />
                             <div className={styles.nowPlayingDetails}>
                                 
